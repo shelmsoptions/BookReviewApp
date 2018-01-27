b78af551e4912b07db2ac6dccae9beb7e999407c
Scott Helms <shelms@imageoptions.net>
2018-01-27 13:26:24 -0800

    BRA-001 include and config bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
   devise_for :users
   authenticate :user do
     root to: 'users#index'
