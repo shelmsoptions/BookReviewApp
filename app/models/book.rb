56d7dff5bde74b74248a5c349ca83c0836abb1c4
Scott Helms <shelms@imageoptions.net>
2018-02-26 14:07:50 -0800

    BRA-005 fix delete from review index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :author
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   
   default_scope { order('created_at ASC') }
   
